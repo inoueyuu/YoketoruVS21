11abf430f0a94dde8d13edfc7fac9ce09c8e725e
A601_03 <tnk.dat.class@gmail.com>
2021-07-01 10:37:25 +0900

    画面遷移

--- YoketoruVS21/Form1.Designer.cs
+++ YoketoruVS21/Form1.Designer.cs
@@ -29,12 +29,17 @@ namespace YoketoruVS21
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.startButton = new System.Windows.Forms.Button();
             this.titleLabel = new System.Windows.Forms.Label();
             this.copyrightLabel = new System.Windows.Forms.Label();
             this.TimeLabel = new System.Windows.Forms.Label();
             this.starLabel = new System.Windows.Forms.Label();
             this.hiLabel = new System.Windows.Forms.Label();
+            this.gameoverLabel = new System.Windows.Forms.Label();
+            this.clearLabel = new System.Windows.Forms.Label();
+            this.TitleButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // startButton
@@ -47,6 +52,7 @@ namespace YoketoruVS21
             this.startButton.TabIndex = 0;
             this.startButton.Text = "Game Start";
             this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // titleLabel
             // 
@@ -98,12 +104,54 @@ namespace YoketoruVS21
             this.hiLabel.TabIndex = 5;
             this.hiLabel.Text = "High Score 100";
             // 
+            // gameoverLabel
+            // 
+            this.gameoverLabel.AutoSize = true;
+            this.gameoverLabel.Font = new System.Drawing.Font("Yu Gothic UI", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gameoverLabel.ForeColor = System.Drawing.Color.Blue;
+            this.gameoverLabel.Location = new System.Drawing.Point(303, 191);
+            this.gameoverLabel.Name = "gameoverLabel";
+            this.gameoverLabel.Size = new System.Drawing.Size(199, 48);
+            this.gameoverLabel.TabIndex = 6;
+            this.gameoverLabel.Text = "Game Over";
+            // 
+            // clearLabel
+            // 
+            this.clearLabel.AutoSize = true;
+            this.clearLabel.Font = new System.Drawing.Font("Yu Gothic UI", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clearLabel.ForeColor = System.Drawing.Color.Red;
+            this.clearLabel.Location = new System.Drawing.Point(348, 191);
+            this.clearLabel.Name = "clearLabel";
+            this.clearLabel.Size = new System.Drawing.Size(101, 48);
+            this.clearLabel.TabIndex = 7;
+            this.clearLabel.Text = "Clear";
+            // 
+            // TitleButton
+            // 
+            this.TitleButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.TitleButton.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TitleButton.Location = new System.Drawing.Point(348, 295);
+            this.TitleButton.Name = "TitleButton";
+            this.TitleButton.Size = new System.Drawing.Size(202, 109);
+            this.TitleButton.TabIndex = 8;
+            this.TitleButton.Text = "タイトルへ";
+            this.TitleButton.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TitleButton);
+            this.Controls.Add(this.clearLabel);
+            this.Controls.Add(this.gameoverLabel);
             this.Controls.Add(this.hiLabel);
             this.Controls.Add(this.starLabel);
             this.Controls.Add(this.TimeLabel);
@@ -125,6 +173,11 @@ namespace YoketoruVS21
         private System.Windows.Forms.Label TimeLabel;
         private System.Windows.Forms.Label starLabel;
         private System.Windows.Forms.Label hiLabel;
+        private System.Windows.Forms.Label gameoverLabel;
+        private System.Windows.Forms.Label clearLabel;
+        private System.Windows.Forms.Button Title;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button TitleButton;
     }
 }
 
